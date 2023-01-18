6267724e4c1a96f96a1a90a2c278726a632185ff
PolishBookworm <43704030+PolishBookworm@users.noreply.github.com>
2023-01-18 19:23:01 +0100

    New, turns out to be slower v of bez_kontekstu.cpp

--- olympiad/bez_kontekstu.cpp
+++ olympiad/bez_kontekstu.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
 #include <vector>
+
+bool isAnagram(std::string anagram, char *str, int *len);
 
 int main()
 {
@@ -10,28 +12,32 @@ int main()
 
 	int n, m;
 	std::cin >> n >> m;
-	char msg[n];
-	char frag[m];
+	std::string msg;
+	std::string frag;
 	std::cin >> msg >> frag;
-	std::sort(frag, frag + m);
 
 	std::vector<int> results;
 	for (auto i = 0; i <= n - m; i ++)
 	{
-		char cache[m];
-		for (auto j = 0; j < m; j ++) cache[j] = msg[j+i];
-		std::sort(cache, cache + m);
-		bool isOk = 1;
-		for (auto j = 0; j < m; j ++) if (frag[j] != cache[j])
-		{
-			isOk = 0;
-			break;
-		}
-		if (isOk) results.push_back(i+1);
+		if (isAnagram(frag, &msg[i], &m)) results.push_back(i+1);
 	}
 
 	std::cout << results.size() << '\n';
 	for (auto i : results) std::cout << i << ' ';
 
 	return 0;
+}
+
+bool isAnagram(std::string original, char *str, int *len)
+{
+	for (auto i = 0; i < *len; i ++) for (auto j = 0; j < original.length(); j ++)
+	{
+		if (str[i] == original[j])
+		{
+			original = original.substr(0,j) + original.substr(j+1);
+			break;
+		}
+	}
+	if (original != "") return false;
+	return true;
 }
