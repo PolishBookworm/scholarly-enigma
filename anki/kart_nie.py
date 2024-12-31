b5f76ae1470bf25013dcd7aa53a4aa8d86d20db2
PolishBookworm <oliwka.pszona@gmail.com>
2024-12-31 12:46:40 +0100

    Added scripts for making anki flashcards

---
+++ anki/kart_nie.py
@@ -0,0 +1,15 @@
+with open("kart.txt") as f:
+	data = f.readlines()
+
+for i in range(len(data)):
+	tmp = data[i][::-1]
+	x = tmp.find("-")
+	try:
+		tmp = tmp[0:x] + ";" + tmp[x+1:]
+	except IndexError:
+		tmp = tmp[0:x] + ";"
+	data[i] = tmp[::-1]
+
+with open("kart.txt","w") as f:
+	for datum in data:
+		f.write(datum)
---
+++ anki/poems.py
@@ -0,0 +1,9 @@
+with open("in.txt") as f:
+	data = f.readlines()
+
+data = [datum[:-1] for datum in data]
+
+with open("out.txt", "w") as f:
+	f.write(f"---:{data[0]}\n")
+	for i in range(1,len(data)):
+		f.write(f"{data[i-1]}:{data[i]}\n")
